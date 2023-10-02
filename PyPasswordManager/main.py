aa4bc010ea30e53bdd509b556d02cdd3bcf0f23f
Git-iw <141666971+Git-iw@users.noreply.github.com>
2023-10-02 18:43:10 +0100

    My Commit for Password Manager.

---
+++ PyPasswordManager/main.py
@@ -0,0 +1,41 @@
+# Main file for password manager
+# Babai, idi best kaadu, secureu kaadu, edo showoff ki paduntadhi chesthunna
+# kavali ani savagottaku(Leave me alone Please leave me alone)
+
+# Creating functions for easy code
+
+# View uses for loop in readlines function to get the lines and print them
+def view():
+    with open("passwords.txt", 'r') as fileofPwdsr:
+        for line in fileofPwdsr.readlines():
+            print(line)
+
+# Add uses .write method to write what is given as input
+def add():
+    accountName = input("Enter account name: ")
+    password = input("Enter password: ")
+    with open("passwords.txt", 'a') as fileofPwds:
+        fileofPwds.write("\n" + accountName + " | " + password + "\n")
+
+
+
+# Takes inupt from the user
+masterPassword = input("Enter the Master passsword: ")
+
+
+
+# Condition for validating Master Password
+try:
+    if masterPassword == "F;=pwr{']LM,!Z%%/)!*<lDL":
+        while True:
+            mode = input("Do you want to create a new password or read the existing(add, view, quit): ")
+            if mode == "add":
+                add()
+            elif mode == "view":
+                view()
+            elif mode == "quit":
+                break
+    else:
+        print("Invalid Master Password")
+except:
+    print("Yerri Pappa, cheppindhi cheyyi!")
---
+++ PyPasswordManager/passwordGenerator.py
@@ -0,0 +1,14 @@
+# Password generator
+
+import random
+
+characters = "!£$%^&*()_-=+{}[]~#@':;?/>.<,|qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+lengthofPassword = int(input("Enter the length of the password: "))
+
+password = ''
+
+for i in range(lengthofPassword):
+    password+= random.choice(characters)
+
+print(f"Password: {password}")
