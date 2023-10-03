6fe15407358f8bda3b0e3701c83020bc18b9b2db
Git-iw <141666971+Git-iw@users.noreply.github.com>
2023-10-03 13:09:55 +0100

    Enjoy Playing Rock Paper Scissors.

---
+++ game.py
@@ -0,0 +1,31 @@
+# Simplest RPS game in Python:
+
+import random
+
+gameOptions = ["rock","paper","scissors"]
+
+myInput = input("Enter Your Option: ")
+myInput = myInput.lower()
+computerInput = random.choice(gameOptions)
+
+if myInput == "rock" and computerInput == "scissors":
+    print(f"My Option: {computerInput}")
+    print("You Win!!")
+elif myInput == "scisssors" and computerInput == "rock":
+    print(f"My Option: {computerInput}")
+    print("I Win!!")
+elif myInput == "paper" and computerInput == "rock":
+    print(f"My Option: {computerInput}")
+    print("You Win!!")
+elif myInput == "rock" and computerInput == "paper":
+    print(f"My Option: {computerInput}")
+    print("I Win!!")
+elif myInput == "scissors" and computerInput == "paper":
+    print(f"My Option: {computerInput}")
+    print("You Win!!")
+elif myInput == "paper" and computerInput == "scissors":
+    print(f"My Option: {computerInput}")
+    print("I Win!!")
+else:
+    print("Unnayi ray be cheppesh.")
+
