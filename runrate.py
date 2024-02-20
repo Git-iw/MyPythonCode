020e0a73f54c6c83aebbf36e933eb2106a1ed43f
Git-iw <141666971+Git-iw@users.noreply.github.com>
2024-02-20 11:12:45 +0000

    Checkout the Cricket calcs in Python.

---
+++ runrate.py
@@ -0,0 +1,9 @@
+import sys
+
+try:
+    runsInput = int(input("Runs Scored: "))
+    oversInput = int(input("Overs done: "))
+    runRate = float(runsInput/oversInput)
+    print(f"Run Rate: {runRate}")
+except:
+    sys.exit("Em rayalo thelidha be neeku!")
---
+++ strikerate.py
@@ -0,0 +1,13 @@
+from fractions import Fraction
+
+def strikeRate(balls, runs):
+    try:
+        strike_rate = float((runs/balls) * 100)
+        return strike_rate
+    except:
+        print("Em rayalo thelidha cheppesh!")
+
+
+runsInput = int(input("Runs Scored: "))
+ballsInput = int(input("Balls Scored: "))
+print(strikeRate(ballsInput, runsInput))
